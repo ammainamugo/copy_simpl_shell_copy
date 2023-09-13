7035226b792f99270821441832dc29b0e2285ef2
ammainamugo <ammainamugo@gmail.com>
2023-09-13 13:51:13 -0700

    builtins

---
+++ builtins.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * _shellexit - function to exit the shell program
+ * @info: Pointer to a structure containing of type info_t potential contains
+ * information and arguments related to the shell program
+ * Return: exits with a given integer exit status
+ * (0) if info.argv[0] != "exit"
+ */
+
+int _shellexit(info_t *info)
+{
+	int exit_check;
+
+	if (info->argv[1])
+	{
+		exit_check = _erratoi(info->argv[1]);
+		if (exit_check == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _change_directory - function that changes the current directory
+ * of a shell process
+ * @info: Pointer to a structure of type info_t which presumably contains
+ * arguments related to the shell program
+ * Return: Always 0 for success
+ */
+
+int _change_directory(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _shellhelp - provides help info within the shell program
+ * @info: Pointer to a structure of type info_t as parameter
+ * Return: Always 0
+ */
+
+int _shellhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
