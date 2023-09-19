f6413482fc8bb6bd471117bc1e3efc626b624d14
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-09-19 06:24:49 -0700

    file to print inputted string

--- print_str.c
+++ print_str.c
@@ -17,3 +17,22 @@ void p_string(char *s)
 		i++;
 	}
 }
+
+/**
+ * pfd_string - prints input string to fd
+ * @s: string to be printed
+ * @fd: filedescriptor to write to
+ * Return: total number of chars
+ */
+int pfd_string(char *s, int fd)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		i += wri_fd(*s++, fd);
+	}
+	return (i);
+}
