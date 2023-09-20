d32b1e5c1d29ced1450469e8577ce0eb1d58a84b
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-09-20 09:04:58 -0700

    header file

--- main.h
+++ main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int sh_mode(type_info *sh_info);
-int find_delim(char ch, char *d);
-int find_alpha(int ch);
-
 void p_string(char *s);
 int pfd_string(char *s, int fd);
 int pr_error(char c);
